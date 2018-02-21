17aafafbc46ec928b21185753e17df01857f096f
Pinky Kohsuwan <pkohsuwan@Pinkys-MacBook-Pro.local>
2018-02-20 17:37:38 -0800

    first commit for pics message

---
+++ pinkypics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pinkypics
+//
+//  Created by Pinky Kohsuwan on 2/20/18.
+//  Copyright © 2018 Pinky Kohsuwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
